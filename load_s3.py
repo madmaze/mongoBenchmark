fef4da0b65ca55f052d4290c3462ceade8e6f3b1
Paul O'Neil <poneil1@jhu.edu>
2012-09-12 21:41:49 +0000

    load 1000 s3 files into mongolab

--- load_s3.py
+++ load_s3.py
@@ -1,9 +1,22 @@
 #! /usr/bin/env python
 
 import pymongo
+import gridfs
 import os
 
-file_list = os.listdir('/home/ubuntu/s3/')
+base_dir = '/home/ubuntu/s3/'
+file_list = os.listdir(base_dir)
 
+connection = pymongo.Connection('mongodb://dba:sql@ds037637-a.mongolab.com:37637/mongotest')
+db = connection['mongotest']
+
+grid = gridfs.GridFS(db, 'brains')
+
+i = 0
 for path in file_list:
-	print path
+	f = open(base_dir + path)
+	data = f.read()
+	grid.put(data, filename=path)
+	i += 1
+	if i >= 1000:
+		break
