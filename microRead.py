e63f072f23eab59fbc9fd88514254fe145425e44
Ubuntu <ubuntu@ip-10-112-63-198.ec2.internal>
2012-09-17 23:07:10 +0000

    Commit time

--- microRead.py
+++ microRead.py
@@ -2,19 +2,42 @@
 
 import pymongo
 import sys
+import timeit
 
-user = "user"
-passwd = "whatever"
-server = "ds037627-a.mongolab.com:37627"
-inserts = 50
-if sys.argc > 1 and int(sys.argv[1]):
-  inserts = int(sys.argv[1])
+user = "testusr"
+passwd = "testpw"
+dbname = "testdb"
+#server = "ds037627-a.mongolab.com:37627"
+server = "mongoServ1:27017"
+inserts = 1
+execcnt = 1
+id = 1
+#try:
+#  id = int(sys.argv[1])
+#except:
+#  print "You forgot to give me an ID"
+#  sys.exit(-1)
+#if len(sys.argv) > 2 and int(sys.argv[2]):
+#  inserts = int(sys.argv[2])
 
-conn = pymongo.Connection('mongodb://%s:%s@%s/mongodb' % (user,passwd,server))
+startme = """
+import pymongo
+conn = pymongo.Connection('mongodb://%s:%s@%s/%s')
 
 db = conn['mongodb']
 col = db.collection
+inserts = %d
+id = %d""" % (user,passwd,server,dbname,inserts,id) 
 
+timecode = """ 
 for x in range(0,inserts):
-	col.insert({str(x):str(x)}, safe = True)
-print "Successfully tried %d inserts" % inserts
+        print col.find_one()
+#        print col.count()
+#	if not (col.find_one(str(id) + str(x)) == x):
+#	  print "Retrieve of %d on %s failed: got %s" % (x,id,col.find_one(str(id) + str(x)))"""
+
+runme = timeit.Timer(stmt=timecode,setup=startme)
+
+res = runme.timeit(number=execcnt)
+
+print "Successfully tried %d retrieves in %f seconds" % (inserts * execcnt,res)
--- microWrite.py
+++ microWrite.py
@@ -4,31 +4,46 @@ import pymongo
 import sys
 import timeit
 
-user = "user"
-passwd = "whatever"
-server = "ds037627-a.mongolab.com:37627"
-inserts = 50
+
+user = "testusr"
+passwd = "testpw"
+dbname = "testdb"
+#server = "ds037627-a.mongolab.com:37627"
+server = "mongoServ1:27017"
+inserts = 5000
 execcnt = 500
+collname = "collection"
 id = 1
 try:
   id = int(sys.argv[1])
 except:
   print "You forgot to give me an ID"
   sys.exit(-1)
-if len(sys.argv) > 2 and int(sys.argv[2]):
-  inserts = int(sys.argv[2])
+#if len(sys.argv) > 2 and int(sys.argv[2]):
+#  inserts = int(sys.argv[2])
 
 startme = """
 import pymongo
-conn = pymongo.Connection('mongodb://%s:%s@%s/mongodb')
+import hashlib
+conn = pymongo.Connection('mongodb://%s:%s@%s/%s')
 
-db = conn['mongodb']
-col = db.collection
-inserts = %d""" % (user,passwd,server,inserts) 
+db = conn['%s']
+col = db.%s
+inserts = %d
+id = %d
+pre = 0""" % (user,passwd,server,dbname,dbname,collname,inserts,id) 
 
-timecode = """ 
+timecode = """
 for x in range(0,inserts):
-	col.insert({str(x):str(x)}, safe = True)"""
+	try:
+	  #hashval = hashlib.sha1()
+#	  hashval.update(str(x))
+	  col.insert({str(pre) + str(id) + str(x):str(x)}, safe = False)
+        except:
+          print "Oops I bombed out"
+          import traceback
+          traceback.print_exc()
+pre += 1 """
 
 runme = timeit.Timer(stmt=timecode,setup=startme)
 
