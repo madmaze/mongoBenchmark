75089fe8787e38ad4274a783694c00aa34cfb560
Paul O'Neil <redballoon36@gmail.com>
2012-09-26 00:23:52 -0400

    Adjust size and spacing of the plots

--- data_analysis/story_graphs.py
+++ data_analysis/story_graphs.py
@@ -19,9 +19,7 @@ parser.add_argument('--output', nargs='?', type=argparse.FileType('w'), default=
 
 args = parser.parse_args()
 
-graph_commands = []
-
-
+# CPU is in light and dark green
 def plot_cpu(input_file, axes):
 	reader = csv.reader(input_file)
 	times = []
@@ -52,7 +50,9 @@ def plot_cpu(input_file, axes):
 	axes.plot(delta_times, usw, color='#66FF66')
 	axes.set_ylabel('CPU Utilization (%)')
 	axes.set_xlabel('Time (s)')
+	axes.set_ylim(top=100)
 
+# Disk is in Yellow and Light Blue
 def plot_disk(input_file, axes):
 	reader = csv.reader(input_file)
 	
@@ -78,6 +78,7 @@ def plot_disk(input_file, axes):
 	axes.set_ylabel('Disk Activity')
 	axes.set_xlabel('Time (s)')
 
+# Network is in Red and Dark Blue
 def plot_net(input_file, axes):
 	reader = csv.reader(input_file)
 	
@@ -103,6 +104,7 @@ def plot_net(input_file, axes):
 	axes.set_ylabel('Network Activity')
 	axes.set_xlabel('Time (s)')
 
+graph_commands = []
 for line in args.input_file:
 	words = line.split()
 	graph_commands.append({'name':words[0], 'file':words[1], 'figures':words[2:]})
@@ -113,28 +115,41 @@ for computer in graph_commands:
 	f.close()
 
 
-fig = pyplot.figure()
+fig = pyplot.figure(figsize=(6,8))
 for (idx, computer) in enumerate(graph_commands):
 	axes = fig.add_subplot(len(graph_commands), 1, idx+1)
+	second_axes = None
+	axes.set_title(computer['name'])
 	separated_stats = {}
 	stat_name_list = computer['figures']
+	equalize_axes = False
 	for (idx, stat_name) in enumerate(stat_name_list):
 		script = subprocess.Popen(['./filterByStat.py', '--type', stat_name], stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 		(out, err) = script.communicate(computer['time_filtered'])
-		#print out
 		separated_stats[stat_name] = out
 		
 		if idx == 1:
 			cur_axes = pyplot.twinx(axes)
+			second_axes = cur_axes
 		else:
 			cur_axes = axes
 		if stat_name == 'cpu':
 			plot_cpu(StringIO.StringIO(out), cur_axes)
 		elif stat_name == 'disk':
 			plot_disk(StringIO.StringIO(out), cur_axes)
+			if stat_name_list[0] == 'net':
+				equalize_axes = True
 		elif stat_name == 'net':
 			plot_net(StringIO.StringIO(out), cur_axes)
-
+			if stat_name_list[0] == 'disk':
+				equalize_axes = True
+	
+	if equalize_axes:
+		max_top = max(axes.get_ylim()[1], second_axes.get_ylim()[1])
+		axes.set_ylim(top=max_top)
+		second_axes.set_ylim(top=max_top)
+		
+pyplot.subplots_adjust(top=0.95, bottom=0.05, hspace=0.7)
 pyplot.show()
 	
 
